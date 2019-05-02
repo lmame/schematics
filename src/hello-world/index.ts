9a823819f7b42f9a32d29e556ccfbc21624f144d
Laurent Matheo <lmame@me.com>
2019-05-02 12:34:21 -0600

    Adding workshop instructions and exercises.

--- src/hello-world/index.ts
+++ src/hello-world/index.ts
@@ -3,13 +3,27 @@ import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function helloWorld(_options: any): Rule {
+interface HelloWorldOptions {
+  name: string,
+  fileExtension: string,
+}
+
+export function helloWorld(_options: HelloWorldOptions): Rule {
   // LMA:: Original code
   // return (tree: Tree, _context: SchematicContext) => {
   //   return tree;
   // };
 
   return (tree: Tree, _context: SchematicContext) => {
-    return tree.create("foo.html", "<h1>Hello World LMA!</h1>");
+    // return tree.create("foo.html", "<h1>Hello World ${_options.name}!</h1>");
+
+    const logger = _context.logger;
+
+    logger.debug("This is a debug message");
+
+    return tree.create(
+        `${_options.name}.${_options.fileExtension}`,
+        `<p>Hi, ${_options.name}!<p>`
+    );
   };
 }
